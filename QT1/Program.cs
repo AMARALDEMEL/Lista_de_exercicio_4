7c1b6f60c1fcc3eb1de7e0e792f1394eba8c17ee
Lucas Amaral Gonçalves <amaralgoncalveslucas@gmail.com>
2024-09-09 09:24:38 -0300

    nova lista

---
+++ QT1/Program.cs
@@ -0,0 +1,78 @@
+﻿/*-------------------------------------------------------------------
+@Lista: 04 - Funções
+Questão 1: Sistema de Cura de Personagem
+• Contextualização: Em um jogo de RPG, o jogador pode usar poções de cura para
+recuperar a vida do seu personagem durante a batalha. A quantidade de vida
+recuperada depende do tipo de poção, do nível do personagem e da quantidade de
+poções usadas.
+• Comando: Crie um programa que receba o tipo de poção (Pequena, Média, Grande), o
+nível do personagem e a quantidade de poções usadas. Crie uma função que calcule a
+quantidade de vida recuperada com base nos seguintes critérios:
+• Poção Pequena: 10 pontos de vida por poção.
+• Poção Média: 20 pontos de vida por poção.
+• Poção Grande: 30 pontos de vida por poção.
+• Adicione 5 pontos adicionais para cada nível do personagem acima de 5.
+• Exiba o total de vida recuperada.
+@Autor: Lucas Amaral Gonçalves
+@Data: 03/09/2024
+---------------------------------------------------------------------*/
+using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+
+        string? tipoP;
+        int nivel, quantP;
+
+        Console.WriteLine("escreva o tipo de poção (pequena, media ou grande): ");
+        tipoP = Console.ReadLine();
+
+        Console.WriteLine("escreva o nível do jogador: ");
+        int.TryParse(Console.ReadLine(), out nivel);
+
+        Console.WriteLine("escreva a quantidade de poções que deseja tomar?: ");
+        int.TryParse(Console.ReadLine(), out quantP);
+
+        int total = Some(nivel, quantP, tipoP);
+        Console.ReadLine();
+
+        Console.WriteLine("a sua pontuação de vida é: " + total);
+
+
+        Console.WriteLine("\nPressione ENTER para fechar.");
+        Console.ReadLine();
+    }
+    static int Some(int nivel, int quantP, string tipoP)
+    {
+        int nivel1 = 0;
+        int total = 0;
+        if (nivel > 5)
+        {
+            nivel = nivel1 = 5;
+        }
+
+        if (tipoP == "pequena")
+        {
+            total = (10 + nivel1) * quantP;
+        }
+
+        if (tipoP == "media")
+        {
+            total = (20 + nivel1) * quantP;
+        }
+
+        if (tipoP == "grande")
+        {
+            total = (30 + nivel1) * quantP;
+        }
+        return total;
+
+
+    }
+
+
+}
---
+++ QT4/Program.cs
@@ -0,0 +1,131 @@
+﻿/*-------------------------------------------------------------------
+@Lista: 04 - Funções
+Questão 4: Sistema de Resgate em Labirinto
+• Contextualização: Em um jogo de exploração, o jogador precisa resgatar aliados perdidos em um labirinto. O sucesso do resgate depende da quantidade de energia, da distância até o aliado e do nível de ameaça no caminho.
+• Comando: Crie um programa que receba a energia do jogador, a distância até o aliado (em metros) e o nível de ameaça (baixo, médio, alto). Crie uma função que determine se o resgate é possível com base nas seguintes condições:
+• Energia maior que 50: Resgate bem-sucedido.
+• Distância menor que 20 metros: +10 de energia.
+• Nível de ameaça:
+o Baixo: Sem penalidade.
+o Médio: -10 de energia.
+o Alto: -20 de energia.
+• Exiba se o resgate foi bem-sucedido ou falhou.
+@Autor: Lucas Amaral Gonçalves
+@Data: 09/09/2024
+---------------------------------------------------------------------*/
+using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+        string? ameaca;
+        int ener, aliado;
+
+        Console.WriteLine("qual o nível de ameaça? (baixo, medio, alto): ");
+        ameaca = Console.ReadLine();
+
+        Console.WriteLine("qual a sua energia inicial?: ");
+        int.TryParse(Console.ReadLine(), out ener);
+
+        Console.WriteLine("qual sua distância até o aliado?: ");
+        int.TryParse(Console.ReadLine(), out aliado);
+
+        int total = Some(aliado, ener, ameaca);
+        Console.ReadLine();
+
+        Console.WriteLine("o seu ataque foi de: " + total);
+
+        Console.WriteLine("\nPressione ENTER para fechar.");
+        Console.ReadLine();
+    }
+    static int Some(int aliado, int ener, string? ameaca)
+    {
+
+        int total = 0;
+        if (ameaca == "baixo")
+        {
+            if (aliado < 20)
+            {
+                total = ener + 10;
+                if (total > 50)
+                {
+                    Console.WriteLine("Resgate bem sucedido");
+                }
+                else
+                {
+                    Console.WriteLine("Resgate falhou");
+                }
+            }
+            else
+            {
+                if (ener > 50)
+                {
+                    Console.WriteLine("Resgate bem sucedido");
+                }
+                else
+                {
+                    Console.WriteLine("Resgate falhou");
+                }
+            }
+        }
+
+        else if (ameaca == "medio")
+        {
+            ener -= 10;
+            if (aliado < 20)
+            {
+                total = ener + 10;
+                if (total > 50)
+                {
+                    Console.WriteLine("Resgate bem sucedido");
+                }
+                else
+                {
+                    Console.WriteLine("Resgate Falhou");
+                }
+            }
+            else
+            {
+                if (ener > 50)
+                {
+                    Console.WriteLine("Resgate bem sucedido");
+                }
+                else
+                {
+                    Console.WriteLine("Resgate falhou");
+                }
+            }
+        }
+        else if (ameaca == "alto")
+        {
+            ener -= 20;
+            if (aliado < 20)
+            {
+                total = ener + 10;
+                if (total > 50)
+                {
+                    Console.WriteLine("Resgate bem sucedido");
+                }
+                else
+                {
+                    Console.WriteLine("Resgate Falhou");
+                }
+            }
+            else 
+            {
+                   if(ener > 50)
+                {
+                Console.WriteLine("Resgate bem sucedido");
+                }
+                else 
+                {
+                    Console.WriteLine("Resgate falhou");
+                }
+            }
+        }
+        return total;
+    }
+}
